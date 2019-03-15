5c1ad4d241011f7deb29cba59d3196e07e70a3c4
jeffsonarr-7d756cdddd-5c4v7 <jshurak@gmail.com>
2019-03-15 10:46:10 -0400

    debugging sonarr

--- sonarr.sh
+++ sonarr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [[ $(grep -c 'System.Data.SQLite.SQLiteException' /config/logs/sonarr.txt) -gt 0 ]]; then 
-    exit 1 
-fi
+#if [[ $(grep -c 'System.Data.SQLite.SQLiteException' /config/logs/sonarr.txt) -gt 0 ]]; then 
+#    exit 1 
+#fi
